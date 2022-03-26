3619e4f0e0df87a7673ef0752c0c3970e20ad7a2
Darvisechavarria <yerikechavarria210@gmail.com>
2022-03-26 07:57:45 -0500

    se crea funcion para sumar en Python

---
+++ funcion.py
@@ -0,0 +1,6 @@
+#programa que suma 2 numeros
+def sumarNumeros(numero1,numero2):
+    print(f"la suma es {numero1+numero2}")
+
+sumarNumeros(numero1=int(input('Digite un numero: ')),numero2=int(input('Digite un numero: ')))
+sumarNumeros(numero1=int(input('Digite un numero: ')),numero2=int(input('Digite un numero: ')))
